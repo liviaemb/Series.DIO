4743584c8def4f68aa0e949678287b8a4f130ad3
Lívia Moura <86753330+liviaemb@users.noreply.github.com>
2021-09-16 00:56:04 -0300

    Add files via upload

---
+++ Series.DIO/Enum/Genero.cs
@@ -0,0 +1,24 @@
+namespace DIO.Series
+{
+	public enum Genero
+	{
+		Acao = 1,
+		Anime =2,
+		Aventura = 3,
+		Comedia = 4,
+		Documentario = 5,
+		Drama = 6,
+		Espionagem = 7,
+		Faroeste = 8,
+		Fantasia = 9,
+		Ficcao_Cientifica = 10,
+		Infantil = 11,
+		LGBTQIA =12,
+		Musical = 13,
+		Romance = 14,
+		Suspense = 15,
+		Terror = 16
+		
+
+	}
+}
